5e691969a4a35aaa189a118a05e1f50268e44d28
dijith-481 <dijithdinesh@protonmail.com>
2025-07-20 16:55:12 +0530

    constants todo bundle  staticfiles

---
+++ packages/markweavia-core/src/constants/fetchedDefaults.ts
@@ -0,0 +1,21 @@
+// TODO bundle prism.css
+export const DEFAULT_PRISM_CSS = prism.css;
+export const DEFAULT_KATEX_CSS = katex.css;
+export const DEFAULT_PRISM_JS = prism.js;
+
+export const DEFAULT_FONTS = {
+  Inter: {
+    name: "Inter",
+    weight: "100..1000",
+    style: "normal",
+    display: "swap",
+    encoding: inter.woff2,
+  },
+};
+export const DEFAULT_FONT_CODE = {
+  name: "Iosevka",
+  weight: "400",
+  style: "normal",
+  display: "swap",
+  encoding: iosevka.woff2,
+};
---
+++ packages/markweavia-core/src/constants/index.ts
@@ -0,0 +1,42 @@
+import { Theme } from "../types";
+
+export const PAGE_NUMBER = "{pg}";
+export const headerFooterPositions = [
+  "top-left",
+  "top-center",
+  "top-right",
+  "bottom-left",
+  "bottom-center",
+  "bottom-right",
+];
+
+export const THEMES: Record<string, Theme> = {
+  nordDark: {
+    "--background-color": "#2e3440",
+    "--background-color-secondary": "#3b4252",
+    "--text-color": "#d8dee9",
+    "--primary-color": "#81a1c1",
+    "--secondary-color": "#5e81ac",
+  },
+  nordLight: {
+    "--background-color": "#d8dee9",
+    "--background-color-secondary": "#e5e9f0",
+    "--text-color": "#1F2937",
+    "--primary-color": "#5E81AC",
+    "--secondary-color": "#81a1c1",
+  },
+  trueWhite: {
+    "--background-color": "#FFFFFF",
+    "--background-color-secondary": "#cccccc",
+    "--text-color": "#212121",
+    "--primary-color": "#121212",
+    "--secondary-color": "#121212",
+  },
+  trueBlack: {
+    "--background-color": "#121212",
+    "--background-color-secondary": "#2c2c2c",
+    "--text-color": "#E0E0E0",
+    "--primary-color": "#fefefe",
+    "--secondary-color": "#efefef",
+  },
+};
